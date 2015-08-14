a1e85c1bb4299bea94d883f6100e4952223aae83
Carlos Garcell <c.garcell91@gmail.com>
2015-08-13 23:55:01 -0400

    Disable Jenssenger's MongoDB ServiceProvider

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Jenssegers\Monogdb\MongodbServiceProvider'
+		// 'Jenssegers\Monogdb\MongodbServiceProvider'
 	),
 
 	/*
